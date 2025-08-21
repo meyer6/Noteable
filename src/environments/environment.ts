265b1ed80d64a7e31ca871396082c553888c2a2a
meyer6 <69467554+meyer6@users.noreply.github.com>
2025-08-21 01:25:16 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyCRGSBFw1jzoCbKe768HLlZBU4dXY1iMf4",
-        authDomain: "noteable-9ebe5.firebaseapp.com",
-        projectId: "noteable-9ebe5",
-        storageBucket: "noteable-9ebe5.appspot.com",
-        messagingSenderId: "225580247983",
-        appId: "1:225580247983:web:e97d7a4017f119264cdd5a",
-        measurementId: "G-D9VM0ELJRT"
+
+
+
+
+
+        
     }
 };
 
